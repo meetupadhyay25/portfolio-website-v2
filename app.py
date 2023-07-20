508c035dce0a39d545f69b05168bdc4c95ad3c8a
meetupadhyay25 <111427109+meetupadhyay25@users.noreply.github.com>
2023-07-20 09:55:26 +0000

    added database

--- app.py
+++ app.py
@@ -1,40 +1,19 @@
-from flask import Flask,  render_template , jsonify
+from flask import Flask,  render_template 
+from database import load_jobs_from_db
+
 
 app = Flask(__name__)
 
-JOBS = [
-  {
-    'id' : 1,
-    'title' : "Data Engineer",
-    'location' : "Bengaluru, India",
-    'salary' : "10,00,000",
-  },
-  {
-    'id' : 2,
-    'title' : "Data Scientist",
-    'location' : "Delhi, India",
-    'salary' : "20,00,000",
-  },
-  {
-    'id' : 3,
-    'title' : "Front End Developer",
-    'location' : "Mumabi, India",
-    'salary' : "15,00,000",
-  },
-  {
-    'id' : 4,
-    'title' : "Backend Developer",
-    'location' : "Mumabi, India",
-  }
-]
+
 
 @app.route("/")
 def hello_world():
-    return render_template("home.html", jobs = JOBS , company_name = "Test")
+    jobs = load_jobs_from_db
+    return render_template("home.html", jobs = jobs, company_name = "Test")
 
-@app.route("/api/jobs")
-def list_jobs():
-    return jsonify(JOBS)
+# @app.route("/api/jobs")
+# def list_jobs():
+#     return jsonify(JOBS)
 
 if __name__ == "__main__":
   app.run(host="0.0.0.0", debug=True)
